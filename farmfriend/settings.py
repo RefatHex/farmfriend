7180b1f0a6d6a0e0a70ae0d04f56d1106beed3de
refathex <khondakar00@gmail.com>
2025-01-01 03:27:29 +0600

    added apps

--- farmfriend/settings.py
+++ farmfriend/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'billing',
+    'farmers',
+    'storage',
+    'rentals',
+    'consultations',
+    'notifications',
+    'payments',
+    'feedback',
+    'ai_responses',
 ]
 
 MIDDLEWARE = [
