029dd9af0732ff2b43975f665b675c6e4236b733
refathex <khondakar00@gmail.com>
2025-01-11 03:55:11 +0600

    fixed feedback api

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -4,4 +4,6 @@ from .models import Feedback
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = ['content', 'rating', 'review_type', 'created_at']
+        fields = [ 'user', 'target_user', 'gig_id', 'consultation_id',
+            'content', 'rating', 'review_type', 'created_at'
+        ]
--- feedback/urls.py
+++ feedback/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import FeedbackViewSet
 
 router = DefaultRouter()
-router.register(r'feedback', FeedbackViewSet, basename='feedback')
+router.register(r'feedbacks', FeedbackViewSet, basename='feedbacks')
 
 urlpatterns = router.urls
