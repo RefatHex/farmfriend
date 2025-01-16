7ddad195548867e4a708cb32848bd360d55b4bc2
NabiulIslamNabil <nabilnabiulislam@gmail.com>
2025-01-16 16:58:33 +0600

    Changed Storage gig urlname

--- storage/urls.py
+++ storage/urls.py
@@ -3,7 +3,7 @@ from .views import StorageOwnerViewSet, StorageOwnerGigsViewSet, StorageDealsVie
 
 router = DefaultRouter()
 router.register(r'storage-owners', StorageOwnerViewSet, basename='storage-owners')
-router.register(r'storage-owner-gigs', StorageOwnerGigsViewSet, basename='storage-owner-gigs')
+router.register(r'storage-gigs', StorageOwnerGigsViewSet, basename='storage-gigs')
 router.register(r'storage-deals', StorageDealsViewSet, basename='storage-deals')
 
 urlpatterns = router.urls
